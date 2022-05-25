63f70bbcb7d35dda9f5f0d2abf54b5a86f92c340
Scott Christensen <Masqchr@gmail.com>
2022-05-25 14:33:38 -0700

    Attempted Sword Fix

--- Items/Weapons/BHSword.cs
+++ Items/Weapons/BHSword.cs
@@ -21,7 +21,10 @@ namespace Misc.Items.Weapons
 
         public override void OnHitNPC(Player player, NPC target, int damage, float knockBack, bool crit)
         {
-            pumpkinSword(player, target.whoAmI, damage, knockBack);
+            if (target.value > 0f || (target.damage > 0 && !target.friendly))
+            {
+                pumpkinSword(player, target.whoAmI, (int)(damage * 1.5), knockBack);
+            }
         }
 
         private void pumpkinSword(Player player, int i, int dmg, float kb)
@@ -48,13 +51,13 @@ namespace Misc.Items.Weapons
             num2 += (int)player.position.Y;
             float num3 = 8f;
             Vector2 vector = new Vector2((float)num, (float)num2);
-            float num4 = Main.npc[i].position.X - vector.X * 20;
-            float num5 = Main.npc[i].position.Y - vector.Y * 20;
+            float num4 = Main.npc[i].position.X - vector.X;
+            float num5 = Main.npc[i].position.Y - vector.Y;
             float num6 = (float)System.Math.Sqrt((double)(num4 * num4 + num5 * num5));
             num6 = num3 / num6;
             num4 *= num6;
             num5 *= num6;
-            Projectile.NewProjectile((float)num / 2, (float)num2 / 2, num4, num5, ModContent.ProjectileType<Projectiles.BHSwordProj>(), dmg, kb, player.whoAmI, (float)i, 0f);
+            Projectile.NewProjectile((float)num, (float)num2, num4, num5, ModContent.ProjectileType<Projectiles.BHSwordProj>(), dmg, kb, player.whoAmI, (float)i, 0f);
         }
     }
 }
